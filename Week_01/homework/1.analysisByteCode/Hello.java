d690e290b7118bdb4b70a951d1bee6e4d0cafe05
zrzhao <1035168143@qq.com>
2021-01-10 19:13:59 +0800

    作业第一道题

--- Week_01/homework/1.analysisByteCode/Hello.java
+++ Week_01/homework/1.analysisByteCode/Hello.java
@@ -1,13 +1,21 @@
 public class Hello {
     public static void main(String[] args) {
-        byte b = 1;
-        short s = (short) (b + 1);
-        int i = s - 1;
+        byte b = 110;
+        System.out.println(b);
+        short s = (short) (b + 256);
+        System.out.println(s);
+        int i = s - 23;
+        System.out.println(i);
         float f = i + 0.1f;
-        long l = i * 2;
+        System.out.println(f);
+        long l = i * 15;
+        System.out.println(l);
         double d = l / 0.1;
+        System.out.println(d);
         boolean bl = true;
+        System.out.println(bl);
         char c = 'a';
+        System.out.println(c);
         System.out.println(c % i);
         if (d > l) {
             System.out.println(d);
