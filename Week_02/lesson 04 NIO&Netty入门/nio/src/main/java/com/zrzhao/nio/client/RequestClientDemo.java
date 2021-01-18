0f463838a31aa4d12f900b088515b4647c4621fa
赵泽润 <zhaozerun@nxin.com>
2021-01-18 14:17:07 +0800

    提交第四节课作业

---
+++ Week_02/lesson 04 NIO&Netty入门/nio/src/main/java/com/zrzhao/nio/client/RequestClientDemo.java
@@ -0,0 +1,34 @@
+package com.zrzhao.nio.client;
+
+/**
+ * 访问客户端
+ *
+ * @author zrzhao
+ * @date 2021/1/18
+ */
+public class RequestClientDemo {
+
+    public static void main(String[] args) {
+        // 单线程处理
+        String url = "http://localhost:8801";
+        String result = HttpClientUtil.getInstance().requestGet(url);
+        System.out.println(result);
+
+        // 每次都new线程处理
+        url = "http://localhost:8802";
+        result = HttpClientUtil.getInstance().requestGet(url);
+        System.out.println(result);
+
+        // 线程池处理
+        url = "http://localhost:8803";
+        result = HttpClientUtil.getInstance().requestGet(url);
+        System.out.println(result);
+
+        // netty
+        url = "http://localhost:8808/test";
+        result = HttpClientUtil.getInstance().requestGet(url);
+        System.out.println(result);
+    }
+
+
+}
