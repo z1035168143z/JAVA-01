8fc6887ca0feca2bbac16c4b84273802f6f04928
赵泽润 <zhaozerun@nxin.com>
2021-01-19 19:16:42 +0800

    提交测试数据

---
+++ Week_02/lesson 03 GC&JVM调优/homework02/performServer.bat
@@ -0,0 +1,10 @@
+@echo off
+
+:wmic process where "commandline like '%%-jar gateway%%'" call terminate
+for %%I in (UseSerialGC,UseParallelGC,UseConcMarkSweepGC,UseG1GC) do (
+	for %%J in (256m,512m,1g,2g,4g) do (
+		START "gateway" java -Xms%%J -Xmx%%J -XX:+PrintGCDetails -Xloggc:server-%%I-%%J.log -XX:+%%I -XX:-UseAdaptiveSizePolicy -jar gateway-server-0.0.1-SNAPSHOT.jar
+	    sb -u http://localhost:8088/api/hello -c 20 -N 20 > server-sb-%%I-%%J.log
+    )
+)
+pause
